aa03c44e787b533f6fe465ee6ba40eb31fa64d0d
Benjamin Volokh <bevolokh2018@bevolokh2018.local>
2015-09-24 15:15:40 -0700

    Answer 2

--- 2_Plane-Speaking/app.js
+++ 2_Plane-Speaking/app.js
@@ -13,6 +13,12 @@ var xPlanes = [
 var e = document. //This line is unfinished! Use a method to capture our <ul> element.
 
 for(var i=0; i<xPlanes.length; i++) {
-  var xPlane = xPlanes[i]; //What does "i" refer to?
+  var xPlane = xPlanes[i];
+  var listElement = document.createElement("li");
+  listElement.innerHTML = xPlane;
+  parent.appendChild(listElement);//What does "i" refer to? "i" refers to a variable
+  //that increments every "round" of the for loop and set xPlanes euqal to
+  //xPlanes item i. The for loop stops when i is equal to the length of xPlanes.
+
   // Do some things to add <li> elements to our empty list.
 }
