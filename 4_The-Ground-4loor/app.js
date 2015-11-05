06b3dd775af3821544420cc4167148f0f92e5b05
Benjamin Volokh <bevolokh2018@bevolokh2018.local>
2015-11-05 14:47:20 -0800

    Hi

--- 4_The-Ground-4loor/app.js
+++ 4_The-Ground-4loor/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $("#contact-form").submit(function(event){
-    event.preventDefault();
-    $("#textOutput").innerHTML(
-      $("#firstname").html(), $("#lastname").html(), $("#email").html(), $("#message").html()
+    //event.preventDefault();
+    //$("#textOutput").innerHTML(
+    console.log($("#firstname").html() + $("#lastname").html() + $("#email").html() + $("#message").html());
     );
   })
 });
