ba6c2ab23a113d8d7d213615c013c7d7f65a61bb
Benjamin Volokh <bevolokh2018@bevolokh2018.local>
2015-12-03 07:57:39 -0800

    Into-the-Fire is done

--- 5_Into-the-Fire/js/feedback.js
+++ 5_Into-the-Fire/js/feedback.js
@@ -1,10 +1,4 @@
-var ref = new Firebase("https://into-the-fire.firebaseio.com/"); //Use your app's Firebase URL
-
-ref.on("value", function(snapshot) {
-  console.log(snapshot.val());
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
+var ref = new Firebase("https://into-the-fire.firebaseio.com/");
 
 $(document).ready(function(){
   console.log("Document loaded");
@@ -17,7 +11,28 @@ $(document).ready(function(){
       text:$("#text").val(),
       timestamp:new Date().getTime()
     }
+    ref.push(data);
     console.log("Form submitted!");
-    $("<div>").innerHTML =
   });
+
+  function updateForm(form) {
+    $("#widgets").html("");
+    for (var f in form) {
+      var formInput = form[f];
+      var newLi = $(document.createElement("li"));
+      newLi.append("<h1>" + formInput.timestamp + "</h1>");
+      newLi.append("<h2>" + formInput.first + " " + formInput.last + "</h2>");
+      newLi.append("<p>" + formInput.email + "</p>");
+      newLi.append("<p>" + formInput.text + "</p>");
+      $("#inputStorage").append(newLi);
+    }
+  }
+
+  ref.on("value", function(snapshot) {
+    console.log(snapshot.val());
+    updateForm(snapshot.val());
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+
 });
